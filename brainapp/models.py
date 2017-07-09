fd36d900cadd9baa37c70712301d6d06835366b9
petrchmelar179 <petrchmelar179@gmail.com>
2017-07-09 15:59:44 +0200

    Add commented tables into DB and migrate it.

--- brainapp/models.py
+++ brainapp/models.py
@@ -1,39 +1,39 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_security import RoleMixin, UserMixin
 
 db = SQLAlchemy()
-import os
-import shutil
-# roles_users = db.Table(
-#     'roles_users',
-#     db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-#     db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
-# )
-#
-# categories_files = db.Table(
-#     'categories_files',
-#     db.Column('file_id', db.Integer(), db.ForeignKey('file.id')),
-#     db.Column('category_id', db.Integer(), db.ForeignKey('category.id')),
-# )
-#
-# class Role(db.Model, RoleMixin):
-#     id = db.Column(db.Integer(), primary_key=True)
-#     name = db.Column(db.String(80), unique=True)
-#     description = db.Column(db.String(255))
-#
-#     def __str__(self):
-#         return self.name
-#
-# class User(db.Model, UserMixin):
-#     id = db.Column(db.Integer, primary_key=True, )
-#     email = db.Column(db.String(255), unique=True, nullable=False)
-#     password = db.Column(db.String(255), nullable=False)
-#     active = db.Column(db.Boolean())
-#     confirmed_at = db.Column(db.DateTime())
-#     roles = db.relationship('Role', secondary=roles_users,
-#                             backref=db.backref('users', lazy='dynamic'))
-#     def __str__(self):
-#         return self.email
 
+roles_users = db.Table(
+    'roles_users',
+    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
+)
+
+categories_files = db.Table(
+    'categories_files',
+    db.Column('file_id', db.Integer(), db.ForeignKey('file.id')),
+    db.Column('category_id', db.Integer(), db.ForeignKey('category.id')),
+)
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+    def __str__(self):
+        return self.name
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True, )
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+    def __str__(self):
+        return self.email
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -46,12 +46,12 @@ class Category(db.Model):
 class File(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
-    # defined_name = db.Column(db.String(255), nullable=False)
-    # expiration_date = db.Column(db.DateTime, nullable=False)
-    # category_id = db.Column(db.Integer(), db.ForeignKey(Category.id), nullable=True)
-    # category = db.relationship(Category, backref='File')
-    # user_id = db.Column(db.Integer(), db.ForeignKey(User.id))
-    # user = db.relationship(User)
+    defined_name = db.Column(db.String(255), nullable=False)
+    expiration_date = db.Column(db.DateTime, nullable=False)
+    category_id = db.Column(db.Integer(), db.ForeignKey(Category.id), nullable=True)
+    category = db.relationship(Category, backref='File')
+    user_id = db.Column(db.Integer(), db.ForeignKey(User.id))
+    user = db.relationship(User)
 
     def __str__(self):
         return self.name
