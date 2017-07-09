836825ce994a7d8bac470334ace28987763464df
petrchmelar179 <petrchmelar179@gmail.com>
2017-07-09 19:05:37 +0200

    Add admin views into app initialization

--- brainapp/__init__.py
+++ brainapp/__init__.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
-from brainapp.models import db, User, Role
-from flask_security import SQLAlchemyUserDatastore, Security
+from brainapp.models import *
+from brainapp.views import *
+from flask_security import SQLAlchemyUserDatastore, Security, login_required
+from flask_admin import Admin, helpers as admin_helpers
 
+# flask app setup
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
@@ -14,6 +17,13 @@ migrate = Migrate(app, db)
 # flask security setup
 user_data_store = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_data_store)
+
+# flask admin setup
+admin = Admin(app)
+admin.add_view(FileModelView(File, db.session, u'My Files'))
+admin.add_view(FileModelViewAll(File, db.session, u'All Files',endpoint="duplicate_view"))
+admin.add_view(UserModelViewAll(Category, db.session, u'Categories'))
+admin.add_view(SuperUserModelView(User, db.session, u'Users'))
 
 # CLI commands
 @app.cli.command()
@@ -40,3 +50,20 @@ def dbclear():
 def dbinit():
     db.create_all()
     db.session.commit()
+
+
+# views
+@app.route('/')
+@login_required
+def index():
+    return render_template('index.html')
+
+# define a context processor for merging flask-admin's template context into the
+# flask-security views.
+@security.context_processor
+def security_context_processor():
+    return dict(
+        admin_base_template=admin.base_template,
+        admin_view=admin.index_view,
+        h=admin_helpers,
+    )
